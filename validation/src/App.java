5a6a964d01b9c70e706bf082c2aa128bf56fb5ab
Marques <thiago.pires.cs@gmail.com>
2025-02-21 02:16:27 -0300

    Second commit, but now it's on vscode

--- validation/src/App.java
+++ validation/src/App.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String userName = "admin";
+        String password = "password";
+        String inputUsername;
+        String inputPassword;
+
+        while (true) {
+
+            System.out.println("Type your login: ");
+            inputUsername = scanner.nextLine();
+
+            System.out.println("Please, type your passowrd: ");
+            inputPassword = scanner.nextLine();
+
+            if (inputUsername.equals(userName) && (inputPassword.equals(password))) {
+
+                System.out.println("login successful! ");
+
+             scanner.close();
+                break;
+            } else {
+                System.out.println("login denied, please try again!");
+            }
+
+        }
     }
 }
